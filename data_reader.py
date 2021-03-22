8eddcb82f8ccc560efec95db5ed08286c2c4ee67
WEN Hao <wenh06@gmail.com>
2021-03-22 16:27:28 +0800

    update get_fs function

--- data_reader.py
+++ data_reader.py
@@ -858,7 +858,7 @@ class CINC2021Reader(object):
         return labels
 
 
-    def get_fs(self, rec:str) -> Real:
+    def get_fs(self, rec:str, from_hea:bool=True) -> Real:
         """ finished, checked,
 
         get the sampling frequency of a record
@@ -867,14 +867,20 @@ class CINC2021Reader(object):
         -----------
         rec: str,
             name of the record
+        from_hea: bool, default True,
+            if True, get sampling frequency from corresponding header file of the record;
+            otherwise from `self.fs`
 
         Returns:
         --------
         fs: real number,
             sampling frequency of the record `rec`
         """
-        tranche = self._get_tranche(rec)
-        fs = self.fs[tranche]
+        if from_hea:
+            fs = self.load_ann(rec)["fs"]
+        else:
+            tranche = self._get_tranche(rec)
+            fs = self.fs[tranche]
         return fs
 
     
