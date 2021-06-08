08db5c97d7664a845a42d1c05ea3fab20a3f652c
WEN Hao <wenh06@gmail.com>
2021-06-08 09:15:18 +0800

    add download_blob

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,4 +1,12 @@
 """
+extract data files and header files from corresponding .tar.gz files
+
+since the header files in the WFDB_*.tar.gz files are out-of-date,
+only the header files in the headers-only .tar.gz files will be used
+
+TODO
+----
+add functionality of downloading the .tar.gz files from GCP using `download_blob`
 """
 import os, subprocess, shutil, argparse, tarfile, time
 from glob import glob
@@ -34,6 +42,9 @@ header_files = [
 
 _tranches = "CPSC,CPSC_Extra,StPetersburg,PTB,PTB_XL,Georgia,CUSPHNFH".split(",")
 
+_url = "https://storage.cloud.google.com/physionetchallenge2021-public-datasets/"
+_source_blob_name = "physionetchallenge2021-public-datasets"
+
 
 def get_parser() -> dict:
     """
@@ -67,6 +78,36 @@ def get_parser() -> dict:
     args = vars(parser.parse_args())
 
     return args
+
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_blob_name = "storage-object-name"
+    # destination_file_name = "local/path/to/file"
+    # https://github.com/GoogleCloudPlatform/python-docs-samples/blob/HEAD/storage/cloud-client/storage_download_file.py
+
+    try:
+        from google.cloud import storage
+    except:
+        raise ModuleNotFoundError("one should install the package \042google\042")
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+
+    # Construct a client side representation of a blob.
+    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
+    # any content from Google Cloud Storage. As we don't need additional data,
+    # using `Bucket.blob` is preferred here.
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        "Blob {} downloaded to {}.".format(
+            source_blob_name, destination_file_name
+        )
+    )
 
 
 def run(input_directory:str,
