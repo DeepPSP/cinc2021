fd4db311fd14404262a145b714a06ea2ffa105e2
WEN Hao <wenh06@gmail.com>
2021-11-20 00:09:51 +0800

    preparing to perform NAS

---
+++ nas.py
@@ -0,0 +1,39 @@
+"""
+"""
+
+from typing import NoReturn, Optional, Tuple, Sequence
+
+import torch
+import torch.nn as nn
+from torch.nn.parallel import DistributedDataParallel as DDP, DataParallel as DP
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from easydict import EasyDict as ED
+
+from torch_ecg.torch_ecg.utils.nas import NAS
+
+from trainer import CINC2021Trainer
+from dataset import CINC2021
+
+
+__all__ = ["CINC2021NAS"]
+
+
+class CINC2021NAS(NAS):
+    """
+    """
+    __name__ = "CINC2021NAS"
+
+    def __init__(self,
+                 train_config:dict,
+                 model_configs:Sequence[dict],) -> NoReturn:
+        """ finished, NOT checked,
+
+        Parameters
+        ----------
+        train_config: dict,
+            train configurations
+        model_configs: sequence of dict,
+            model configurations, each with a different network architecture
+        """
+        super().__init__(CINC2021Trainer, CINC2021, train_config, model_configs)
